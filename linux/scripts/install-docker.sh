6a5a01ec3af722f820d65c9a34583b1cc52d36f3
tmagalhaes1985 <thiago.magalhaes@gmail.com>
2020-02-14 12:09:32 -0300

    Update scripts

--- linux/scripts/install-docker.sh
+++ linux/scripts/install-docker.sh
@@ -1,17 +1,44 @@
-# !/bin/bash
+#!/bin/bash
 
-# Download and install Docker
+# Docker
 curl -fsSL https://get.docker.com -o get-docker.sh
+
 sudo sh get-docker.sh
-
-# Install Docker dependencies and repositories
-# dnf -y install dnf-plugins-core
-# dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-# dnf -y install docker-ce docker-ce-cli containerd.io
-
-# Enable and start Docker service
-sudo systemctl enable docker
-sudo systemctl start docker
-
-# Add your user to the "docker" group to use Docker as a non-root user
 sudo usermod -aG docker $USER
+sudo systemctl enable docker && sudo systemctl start docker
+
+# Docker Compose
+
+# Check at https://github.com/docker/compose/releases/latest which is the latest version available
+COMPOSE_VERSION="1.25.4"
+
+sudo curl -L "https://github.com/docker/compose/releases/download/${COMPOSE_VERSION}/docker-compose-Linux-x86_64" -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+
+: '
+# Docker Stacks
+
+Docker                 | Easy Stack             | Self-Hosted Stack
+-----------------------|------------------------|------------------
+Swarm GUI              | Portainer              | Portainer
+Central Monitoring     | Librato, Sysdig        | Prometheus + Grafana
+Central Logging        | Docker for AWS/Azure   | ELK
+Layer 7 Proxy          | Flow-Proxy, Traefik    | Flow-Proxy, Traefik
+Registry               | Docker Hub, Quay       | Docker Distribution + Portus
+CI/CD                  | Codeship, TravisCI     | Jenkins
+Storage                | Docker for AWS/Azure   | REX-Ray
+Networking             | Docker Swarm           | Docker Swarm
+Orchestration          | Docker Swarm           | Docker Swarm
+Runtime                | Docker                 | Docker
+HW/OS                  | Docker for AWS/Azure   | InfraKit, Terraform
+
+# Docker path comparison between Linux and Windows
+
+Use case               |  Linux                 | Windows
+-----------------------|------------------------|--------
+client binary          |  /usr/bin/docker       |  C:\Program Files\Docker\docker.exe
+daemon binary          |  /usr/bin/dockerd      |  C:\Program Files\Docker\dockerd.exe
+server configuration   |  /etc/docker           |  C:\ProgramData\docker\config
+install dir            |  /var/lib/docker       |  C:\ProgramData\docker
+hosts file             |  /etc/hosts            |  C:\Windows\System32\drivers\etc\hosts
+'
